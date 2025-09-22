0be2c1abb0b5b612f4c864b2a81f7c3316857fbc
Partha Mazumder <partha2020mz02@gmail.com>
2025-09-22 11:04:56 +0600

    Problem 43 to 48

---
+++ Programming Challenge/src/_44_PalindromeArray.java
@@ -0,0 +1,26 @@
+/*
+Create a program to check is the array is palindrome or not
+ */
+
+public class _44_PalindromeArray {
+    public static void main(String[] args) {
+        int[] numArr = ArrayUtility.inputArray();
+        boolean isPalin = isPalindrome(numArr);
+        if (isPalin) {
+            System.out.println("Your array is Palindrome");
+        } else {
+            System.out.println("Your array is not Palindrome");
+        }
+    }
+
+    public static boolean isPalindrome(int[] numArr) {
+        int i = 0;
+        while (i < numArr.length / 2) {
+            if (numArr[i] != numArr[numArr.length - 1 -i]) {
+                return false;
+            }
+            i++;
+        }
+        return true;
+    }
+}
