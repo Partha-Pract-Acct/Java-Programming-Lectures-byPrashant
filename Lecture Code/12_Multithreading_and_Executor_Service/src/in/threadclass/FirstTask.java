933b653b091bf5ef9e398234195fa6752ebacf1b
Partha Mazumder <partha2020mz02@gmail.com>
2026-01-02 12:48:36 +0600

    Lecture 12 to 12.2

---
+++ Lecture Code/12_Multithreading_and_Executor_Service/src/in/threadclass/FirstTask.java
@@ -0,0 +1,12 @@
+package in.threadclass;
+
+public class FirstTask extends Thread {
+    @Override
+    public void run() {
+        // First task
+        for (int i = 1; i < 1000; i++) {
+            System.out.printf("%d* ", i);
+        }
+        System.out.printf("\n %s * task completed", Thread.currentThread().getName());
+    }
+}
